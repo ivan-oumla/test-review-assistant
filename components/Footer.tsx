3f2b0f1da335830350eabf15e64269b160fc4eeb
Ivan Saldaño <i.saldano@oumla.com>
2026-02-16 17:43:25 +0900

    Add footer component to home page

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 py-4">
+      <div className="container mx-auto px-4 text-center text-sm text-gray-600">
+        <p>&copy; 2024 Oumla Review Assistant. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
